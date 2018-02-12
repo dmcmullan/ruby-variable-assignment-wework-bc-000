1de1e6bcd54de6c31de8d5dfe97f9d2f36735f94
Dominic McMullan <dominic.mcmullan@wework.com>
2018-02-12 19:41:12 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,9 +1,39 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 greeting = "Hello World"
-
+my_favorite_color = "Orange"
 colors = ["Red", "Blue", 42, "Yellow"]
 
 colors[0]
 colors[2]
-colors[10] = "Grey"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+colors[10] = my_favorite_color
